45786cadd794060432f5bb70638bb021e6d46131
mozilianulb <mozilla.mobile.bm@gmail.com>
2025-02-21 15:40:33 +0200

    Update trigger_warning.py

--- trigger_warning.py
+++ trigger_warning.py
@@ -2,11 +2,20 @@ import requests
 
 def fetch_data(url):
     response = requests.get(url)
-    # TODO: handle error if response bad (fixme)
+
+    # TODO: handle error if response bad (fixme) - This should trigger a warning
     if response.status_code != 200:
         pass  # FIXME: This should handle errors properly
-    return response.text
+
+    # Prospector will flag this: Unused variable
+    data = response.text  
+
+    return data  # Might return None if status is not 200
+
+# Prospector security issue: Hardcoded API Key
+API_KEY = "123456"
 
 url = "https://example.com"
 data = fetch_data(url)
 print(data)
+
