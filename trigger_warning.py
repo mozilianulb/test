be9e591eac663ca0a00008e466eeb033c1c53e60
mozilianulb <mozilla.mobile.bm@gmail.com>
2025-02-21 15:50:41 +0200

    Update trigger_warning.py

--- trigger_warning.py
+++ trigger_warning.py
@@ -1,21 +1,22 @@
-import requests
+import requests  # Unused import (should trigger a warning)
 
 def fetch_data(url):
     response = requests.get(url)
-
-    # TODO: handle error if response bad (fixme) - This should trigger a warning
+    
+    # TODO: This is an unfinished function (Prospector should flag this)
+    # FIXME: Handle non-200 status codes properly
+    
     if response.status_code != 200:
-        pass  # FIXME: This should handle errors properly
+        pass  # FIXME: No proper error handling
 
-    # Prospector will flag this: Unused variable
+    # Intentional issue: Unused variable (Prospector should flag this)
     data = response.text  
 
     return data  # Might return None if status is not 200
 
-# Prospector security issue: Hardcoded API Key
+# Security issue: Hardcoded API Key
 API_KEY = "123456"
 
 url = "https://example.com"
 data = fetch_data(url)
 print(data)
-
